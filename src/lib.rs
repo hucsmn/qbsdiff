105ff15542a4e37f6bc4b4b9bbc199ccf9565b22
hucsmn <hucsmn@hotmail.com>
2019-11-01 19:26:01 +0800

    update to v1.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ Fast and memory saving bsdiff 4.x compatible delta compressor and patcher.
 Add dependency to `Cargo.toml` under your project:
 ```toml
 [dependencies]
-qbsdiff = "1.1"
+qbsdiff = "1.2"
 ```
 
 Build commands
