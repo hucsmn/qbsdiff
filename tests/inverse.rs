dbf652125ae5f22b3c0cfbc0f73eacf3cb0d60c8
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-11-24 04:16:34 +0700

    tests/inverse: Add property-based test

--- tests/inverse.rs
+++ tests/inverse.rs
@@ -2,6 +2,10 @@ mod common;
 
 use common::*;
 
+extern crate quickcheck;
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
 #[test]
 fn invert_samples() {
     let samples = list_samples().unwrap();
@@ -15,4 +19,10 @@ fn invert_samples() {
             panic!("incompatible: qbsdiff/bspatch `{}`", sample.name);
         }
     }
+}
+
+#[quickcheck]
+fn invert_random(s: Vec<u8>, t: Vec<u8>) -> bool {
+    let p = qbsdiff(&s[..], &t[..]).unwrap();
+    return qbspatch(&s[..], &p[..]).unwrap() == t;
 }
